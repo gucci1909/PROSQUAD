964aae3484417e062a5a59a47f7a8393b58cd65e
arnab9593 <arnabpal679@gmail.com>
2022-12-21 19:53:35 +0530

    day 1 release 1

--- pages/signin.js
+++ pages/signin.js
@@ -4,35 +4,32 @@ import "aos/dist/aos.css";
 import { useEffect } from 'react'
 
 export default function Signin() {
-    useEffect(() => {
-        Aos.init({duration:2000})
-       })
-      return (
-        <div className={styles.container}>
-          <meta charSet="UTF-8" />
-          <title>Animated Login Form</title>
-          <link rel="stylesheet" href="style.css" />
-          <div className={styles.box} data-aos='flip-left'>
-            <form autoComplete="off" className={styles.form}>
-              <h2 className={styles.sign}>Sign in</h2>
-              <div className={styles.inputBox}>
-                <input type="text" required="required" />
-                <span>Userame</span>
-                <i />
-              </div>
-              <div className={styles.inputBox}>
-                <input type="password" required="required" />
-                <span>Password</span>
-                <i />
-              </div>
-              <div className={styles.links}>
-                <a href="#">Forgot Password ?</a>
-                <a href="#">Signup</a>
-              </div>
-              <input defaultValue="Login" className={styles.submit}/>
-            </form>
+  useEffect(() => {
+    Aos.init({ duration: 2000 })
+  })
+  return (
+    <div className={styles.container}>
+      <div className={styles.box} data-aos='flip-left'>
+        <form autoComplete="off" className={styles.form}>
+          <h2 className={styles.sign}>Sign in</h2>
+          <div className={styles.inputBox}>
+            <input type="text" required="required" />
+            <span>Username</span>
+            <i />
           </div>
-        </div>
-      );
-    
-  };
+          <div className={styles.inputBox}>
+            <input type="password" required="required" />
+            <span>Password</span>
+            <i />
+          </div>
+          <div className={styles.links}>
+            <a href="#">Forgot Password ?</a>
+            <a href="#">Signup</a>
+          </div>
+          <input defaultValue="Login" className={styles.submit} />
+        </form>
+      </div>
+    </div>
+  );
+
+};
