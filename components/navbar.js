f4f91a5deb6133a2993897003b0f55d05e1ea1d5
arnab9593 <arnabpal679@gmail.com>
2022-12-22 00:36:10 +0530

    day 2 release 1

--- components/navbar.js
+++ components/navbar.js
@@ -1,13 +1,14 @@
 import Link from "next/link";
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Navbar.module.css';
 
 export default function Navbar() {
 
     return (
-        <>
+        <div className={styles.container}>
+            <Link href="/">Home</Link>
             <Link href="signin">Signin</Link>
             <Link href="signup">Signup</Link>
-        </>
+        </div>
     )
 
 }
