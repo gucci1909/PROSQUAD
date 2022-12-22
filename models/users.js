a73ebde5d2ea25751bdd2df7c85ba189c3afa98d
gucci1909 <umangarora0134@gmail.com>
2022-12-22 15:01:16 +0530

    backend

--- models/users.js
+++ models/users.js
@@ -1,34 +1,34 @@
 const mongoose = require("mongoose");
 
 const usersSchema = new mongoose.Schema(
-	{
-		name: { type: String, required: true },
+  {
+    name: { type: String, required: true },
 
-		email: {
-			type: String,
-			unique: true,
-			required: true,
-		},
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
 
-		password: { type: String, required: true },
+    password: { type: String, required: true },
 
-		role: {
-			type: String,
-			enum: ["admin", "student", "teacher"],
-			default: "student",
-		},
+    role: {
+      type: String,
+      enum: ["admin", "student", "teacher"],
+      default: "student",
+    },
 
-		status: {
-			type: String,
-			enum: ["pending", "approved", "rejected"],
-			default: "approved",
-		},
-	},
-	{
-		timestamps: true,
-		versionKey: false,
-	}
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "approved",
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
 );
 
 mongoose.models = {};
-module.exports = mongoose.model("User", usersSchema);
+module.exports = mongoose.model("users", usersSchema);
