a1a9d026f6965519a2b46374c43e96b320ecb84a
gucci1909 <umangarora0134@gmail.com>
2022-12-22 23:19:34 +0530

    single course

--- models/socket.js
+++ models/socket.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+//chattingSchema
 const chatSchema = new mongoose.Schema({
     chat_id : {type:Number,required:true},
     message : {type:String,required:true}
