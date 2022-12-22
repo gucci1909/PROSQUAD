cbabbb51d6c5aada43d06f7306e42db5e546dbd2
pratikderepatil <pratikdere333@gmail.com>
2022-12-22 18:01:38 +0530

    axios

--- pages/signup.js
+++ pages/signup.js
@@ -1,46 +1,26 @@
-import styles from '../styles/Signup.module.css'
+import styles from "../styles/Signup.module.css";
 import Aos from "aos";
 import "aos/dist/aos.css";
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-import Navbar from "../components/navbar";
 import {
-	Button,
-	Center,
-	Divider,
-	Flex,
 	Link,
 	Tab,
 	TabList,
 	TabPanel,
 	TabPanels,
 	Tabs,
-	Text,
 } from "@chakra-ui/react";
 import Form from "../components/Form";
 
-
 export default function Signin() {
-    useEffect(() => {
-        Aos.init({ duration: 2000 })
-    })
+	useEffect(() => {
+		Aos.init({ duration: 2000 });
+	});
 
-    // const [text, setText] = useState({
-    //     name: "",
-    //     email: "",
-    //     password: ""
-    // });
-
-    // const handleForm = (e) => {
-    //     setText(e.target.value);
-    //     console.log(e.target.value);
-    // }
-
-
-
-    const initState = {
-		name: "",
+	const initState = {
+		fullname: "",
 		email: "",
 		password: "",
 		role: "student",
@@ -50,83 +30,57 @@ export default function Signin() {
 		setForm({ ...form, [name]: value });
 	};
 
-	const onClick = () => {
-		setText("");
-	};
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(form);
+		
 	};
 
+	return (
+		<div className={styles.container}>
+			<div className={styles.box} data-aos="flip-left">
+				<form
+					autoComplete="off"
+					className={styles.form}
+					onSubmit={handleSubmit}
+				>
+					<h2 className={styles.sign}>Sign Up</h2>
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.box} data-aos='flip-left'>
-                <form autoComplete="off" className={styles.form}>
-				<h2 className={styles.sign}>Sign Up</h2>
+					<Tabs isFitted variant="enclosed" className={styles.tab}>
+						<TabList>
+							<Tab
+								onClick={() => {
+									setForm({ ...form, role: "student" });
+								}}
+							>
+								Student
+							</Tab>
+							<Tab
+								onClick={() => {
+									setForm({ ...form, role: "teacher" });
+								}}
+							>
+								Teacher
+							</Tab>
+						</TabList>
+						<TabPanels>
+							<TabPanel>
+								<Form {...form} handleInput={handleInput} />
+							</TabPanel>
+							<TabPanel>
+								<Form {...form} handleInput={handleInput} />
+							</TabPanel>
+						</TabPanels>
+					</Tabs>
+					<button className={styles.submit} type="submit">
+						Signup
+					</button>
 
-
-<Tabs isFitted variant="enclosed" className={styles.tab}>
-							<TabList>
-								<Tab 
-									onClick={() => {
-										setForm({ ...form, role: "student" });
-									}}
-								>
-									Student
-								</Tab>
-								<Tab 
-									onClick={() => {
-										setForm({ ...form, role: "teacher" });
-									}}
-								>
-									Teacher
-								</Tab>
-							</TabList>
-							<TabPanels>
-								<TabPanel>
-									<Form {...form} handleInput={handleInput} />
-								</TabPanel>
-								<TabPanel>
-									<Form {...form} handleInput={handleInput} />
-								</TabPanel>
-								{/* <Flex flexDirection={"column"} gap="2" w="90%" m="auto">
-									<Center>
-										<Divider w="30%" />
-										<Text p="2">or continue with</Text>
-										<Divider w="30%" />
-									</Center>
-									<Button
-										colorScheme="whiteAlpha"
-										color={"black"}
-										variant="outline"
-										borderColor={"gray"}
-										leftIcon={<FcGoogle size={"20"} />}
-									>
-										Google
-									</Button>
-									<Button
-										colorScheme="whiteAlpha"
-										color={"black"}
-										variant="outline"
-										leftIcon={<FaGithub size={"20"} />}
-									>
-										GitHub
-									</Button>
-								</Flex> */}
-							</TabPanels>
-						</Tabs>
-						<button className={styles.submit}>Login</button>
-
-                    <div className={styles.links}>
-                        <Link to="#">Already Have Account ?</Link>
-                        <Link href='/signin'>Signin</Link>
-                    </div>
-
-
-                </form>
-            </div>
-        </div>
-    );
-
-};
+					<div className={styles.links}>
+						<Link to="#">Already Have Account ?</Link>
+						<Link href="/signin">Signin</Link>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+}
