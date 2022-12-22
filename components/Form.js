ab4a5c4d0bf27691dd1263896061b91fb93f2e32
Anukriti-Nawani <anunwn@gmail.com>
2022-12-22 12:52:03 +0530

    changes

--- components/Form.js
+++ components/Form.js
@@ -8,6 +8,7 @@ import {
 	InputRightElement,
 } from "@chakra-ui/react";
 import React from "react";
+import styles from "../styles/signup.module.css"
 
 const Form = ({ handleInput, fullname, email, password }) => {
 	const [show, setShow] = React.useState(false);
@@ -15,38 +16,38 @@ const Form = ({ handleInput, fullname, email, password }) => {
 
 	return (
 		<Flex gap="2" flexDirection={"column"}>
-			<FormControl>
+			{/* <FormControl>
 				<FormLabel>Full Name</FormLabel>
 				<Input
 					type="text"
 					name="name"
 					value={fullname}
 					onChange={handleInput}
-				/>
-			</FormControl>
-			<FormControl>
+				/> */}
+				<div className={styles.inputBox}>
+              <input type="email" name="email" value={email} onChange={handleInput} required="required" />
+              <span>Full Name</span>
+              <i />
+            </div>
+			{/* </FormControl>
+			<div className={styles.inputBox}>
 				<FormLabel>Email address</FormLabel>
-				<Input type="email" name="email" value={email} onChange={handleInput} />
-			</FormControl>
-			<FormControl>
-				<FormLabel>Password</FormLabel>
+				<Input type="email" name="email" value={email} onChange={handleInput}/>
+			</div>
+			*/}
+			
+			<div className={styles.inputBox}>
+              <input type="email" name="email" value={email} onChange={handleInput} required="required" />
+              <span>Email</span>
+              <i />
+            </div>
+			<div className={styles.inputBox}>
+              <input type="email" name="email" value={email} onChange={handleInput} required="required" />
+              <span>Password</span>
+              <i />
+            </div>
 
-				<InputGroup size="md">
-					<Input
-						pr="4.5rem"
-						type={show ? "text" : "password"}
-						placeholder="Enter password"
-						value={password}
-						name="password"
-						onChange={handleInput}
-					/>
-					<InputRightElement width="4.5rem">
-						<Button h="1.75rem" size="sm" onClick={handleClick}>
-							{show ? "Hide" : "Show"}
-						</Button>
-					</InputRightElement>
-				</InputGroup>
-			</FormControl>
+			
 		</Flex>
 	);
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '../styles/globals.css'
 import {ChakraProvider} from "@chakra-ui/react"
+import Navbar from '../components/navbar'
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Navbar/>
       <Component {...pageProps} />
     </ChakraProvider>
   )
