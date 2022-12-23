cf5dc91121405ae918c100460b3be6482479ec4c
arnab9593 <arnabpal679@gmail.com>
2022-12-23 10:17:00 +0530

    changes in index page

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
 
-    <div>
+    <div >
       <div>
         <img src="https://www.teachforindia.org/assets/fellowship-desktop.png" alt="image"></img>
       </div>
